c27df1f0f656222b6f9f2233d0aa3b9c22a03e1f
dayserivera <dayserivera@gmail.com>
2020-07-03 22:49:20 -0300

    final seção 11

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
@@ -6,6 +6,8 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.appsdeveloperblog.photoapp.api.users.model.CreateUserRequestModel;
+import com.appsdeveloperblog.photoapp.api.users.model.CreateUserResponseModel;
 import com.appsdeveloperblog.photoapp.api.users.service.UsersService;
 import com.appsdeveloperblog.photoapp.api.users.shared.UserDto;
 
@@ -31,13 +34,15 @@ public class UsersController {
 	}
 	
 	@PostMapping
-	public String createUser(@Valid @RequestBody CreateUserRequestModel userDetail) {
+	public ResponseEntity<CreateUserResponseModel> createUser(@Valid @RequestBody CreateUserRequestModel userDetail) {
 		ModelMapper modelMapper = new ModelMapper();
 		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		
 		UserDto userDto = modelMapper.map(userDetail, UserDto.class);
 		
-		usersService.createUser(userDto);
-		return "Create user method is called";
+		UserDto createdUser = usersService.createUser(userDto);
+		
+		CreateUserResponseModel returnValue = modelMapper.map(createdUser, CreateUserResponseModel.class);
+		return new ResponseEntity<>(returnValue, HttpStatus.CREATED);
 	}
 }
